21b0ebd061b14f9373f96022bc8d6810cbeede83
aios2005 <2005antonioivo@gmail.com>
2026-05-26 16:20:40 -0300

    fix: correction var names

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ let board = ["","","","","","","","",""];
 let currentPlayer = "X";
 let isGameActive = true;
 
-//combinacoes definidas de vitoria
-const WinningCondition = [
+
+const winningConditions = [
     [0,1,2], [3,4,5], [6,7,8], //linhas
     [0,3,6], [1,4,7], [2,5,8], //colunas
     [0,4,8], [2,4,6]           //diagonais
